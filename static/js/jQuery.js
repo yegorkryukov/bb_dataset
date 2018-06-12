6276249677d4c5a63f15393eb323b444f2c3a768
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-06-12 18:06:27 -0400

    Removed custom styles from previous project

--- static/js/jQuery.js
+++
@@ -1,9 +0,0 @@
-console.log('this custom jQuery should work')
-
-// $(document).ready(function(){
-//     $('a, button').click(function() {
-//         console.log('button pressed');
-//         $(this).removeClass('hide');
-//     });
-// });
-
