43305869f2d03606b985a705dbf3e87506dfe53b
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-06-12 18:00:44 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,107 +1,28 @@
-#----------------------------------------------------------------------------#
-# Imports
-#----------------------------------------------------------------------------#
-
-from flask import Flask, render_template, request
-# from flask.ext.sqlalchemy import SQLAlchemy
-import logging
-from logging import Formatter, FileHandler
-from forms import *
+from flask import Flask, render_template, redirect
+from datetime import datetime
+# import pymongo
 import os
 
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
+# Initialize PyMongo to work with MongoDBs
+# MONGODB_URI = os.environ.get('MONGODB_URI')
+# client = pymongo.MongoClient(MONGODB_URI)
 
+# Define database and collection
+# db = client.get_default_database()
+# collection = db.mars_scrape
+
+# initialize Flask app
 app = Flask(__name__)
-app.config.from_object('config')
-#db = SQLAlchemy(app)
-
-# Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
-
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
-#----------------------------------------------------------------------------#
-# Controllers.
-#----------------------------------------------------------------------------#
-
 
 @app.route('/')
-def home():
-    return render_template('pages/placeholder.home.html')
+def index(rerender=False):
+    # data = collection.find_one()
+    #extract table html from string
+    # table = data['facts_table']
+    #replace class and other style params to pickup bootstrap
+    # table = table.replace('dataframe','table').replace('style="text-align: right;','style="text-align: center;')
+    #print(type(data.facts_table))
+    return render_template("index.html", data=data, table=table, rerender=rerender)
 
-
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
-
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
-
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
-# Error handlers.
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    #db_session.rollback()
-    return render_template('errors/500.html'), 500
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
-
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
-#----------------------------------------------------------------------------#
-# Launch.
-#----------------------------------------------------------------------------#
-
-# Default port:
 if __name__ == '__main__':
-    app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    app.run(debug=True, use_reloader=True)
---
+++ static/js/jQuery.js
@@ -0,0 +1,9 @@
+console.log('this custom jQuery should work')
+
+// $(document).ready(function(){
+//     $('a, button').click(function() {
+//         console.log('button pressed');
+//         $(this).removeClass('hide');
+//     });
+// });
+
