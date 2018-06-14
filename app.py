b94dc3d1631c94e36005170615ea82656e589aba
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-06-14 16:33:20 -0400

    Added "OTU" and "Names" routes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import json
 
 # Database Setup
 engine = create_engine("sqlite:///static/data/belly_button_biodiversity.sqlite")
@@ -44,7 +45,27 @@ def names():
         ...
     ]
     """
-    return render_template('names.html')
+    names_of_samples = json.dumps([c.key for c in Samples.__table__.c][1:])
+
+    return render_template('names.html',names_of_samples=names_of_samples)
+
+@app.route('/otu')
+def otu():
+    """List of OTU descriptions.
+
+    Returns a list of OTU descriptions in the following format
+
+    [
+        "Archaea;Euryarchaeota;Halobacteria;Halobacteriales;Halobacteriaceae;Halococcus",
+        "Archaea;Euryarchaeota;Halobacteria;Halobacteriales;Halobacteriaceae;Halococcus",
+        "Bacteria",
+        "Bacteria",
+        "Bacteria",
+        ...
+    ]
+    """
+    otu_descriptions = json.dumps(session.query(OTU.lowest_taxonomic_unit_found).distinct().all())
+    return render_template('otu.html',otu_descriptions=otu_descriptions)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
